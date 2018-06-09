66183aeeb7a9ab7cd9da0481a5971ef156a41f38
Kayla Cohn <kayla.cohn@salesloft.com>
2018-06-09 11:31:47 -0400

    Add coherence configs into router

--- lib/partyplug_web/router.ex
+++ lib/partyplug_web/router.ex
@@ -1,5 +1,6 @@
 defmodule PartyplugWeb.Router do
   use PartyplugWeb, :router
+  use Coherence.Router
 
   pipeline :browser do
     plug :accepts, ["html"]
@@ -7,20 +8,43 @@ defmodule PartyplugWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Coherence.Authentication.Session
+  end
+
+  pipeline :protected do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+    plug Coherence.Authentication.Session, protected: true
   end
 
   pipeline :api do
     plug :accepts, ["json"]
   end
 
-  scope "/", PartyplugWeb do
-    pipe_through :browser # Use the default browser stack
 
+  scope "/", PartyplugWeb do
+    pipe_through :browser
+    coherence_routes()
+  end
+
+  scope "/", PartyplugWeb do
+    pipe_through :protected
+    coherence_routes :protected
+  end
+
+  scope "/", PartyplugWeb do
+    pipe_through :browser
     get "/", PageController, :index
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", PartyplugWeb do
-  #   pipe_through :api
-  # end
+  scope "/", PartyplugWeb do
+    pipe_through :protected
+  end
+
+  scope "/api", PartyplugWeb do
+    pipe_through :api
+  end
 end
